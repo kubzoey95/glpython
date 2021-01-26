e5cd2ce1f46bbd4d31c2a947c446c9ab45de9efa
Jakub Belicki <jakubsliwice@o2.pl>
2021-01-26 17:56:22 +0100

    Update game_object.py

--- my_engine/game_object.py
+++ my_engine/game_object.py
@@ -1,31 +1,100 @@
 import pyrr
-from typing import Tuple
+from typing import Tuple, List, Union, Set
 from numpy import float32
 
 
 class GameObject:
     def __init__(self, translation: Tuple[float] = (0, 0, 0), rotation: Tuple[float] = (0, 0, 0),
-                 scale: Tuple[float] = (1, 1, 1)):
+                 scale: Tuple[float] = (1, 1, 1), name: str = "Untitled"):
         self.__translation = translation
         self.__rotation = rotation
         self.__scale = scale
 
         self.__trans_matrix = pyrr.matrix44.create_from_translation(translation)
+        self.__trans_matrix_valid = True
         self.__rotation_matrix = pyrr.matrix44.create_from_eulers(rotation)
+        self.__rotation_matrix_valid = True
         self.__scale_matrix = pyrr.matrix44.create_from_scale(scale)
+        self.__scale_matrix_valid = True
 
         self.__matrix = self.__trans_matrix @ self.__rotation_matrix @ self.__scale_matrix
 
-        self.__matrix_valid = True
+        self.__children: Set[GameObject] = set()
+        self.__parent: Union[GameObject, None] = None
+        self.name = name
+
+    def __update_matrices(self):
+        if not self.__trans_matrix_valid:
+            self.__trans_matrix = pyrr.matrix44.create_from_translation(self.__translation)
+            self.__trans_matrix_valid = True
+        if not self.__rotation_matrix_valid:
+            self.__rotation_matrix = pyrr.matrix44.create_from_eulers(self.__rotation)
+            self.__rotation_matrix_valid = True
+        if not self.__scale_matrix_valid:
+            self.__scale_matrix = pyrr.matrix44.create_from_scale(self.__scale)
+            self.__scale_matrix_valid = True
 
     @property
     def matrix(self):
-        if self.__matrix_valid:
+        if self.__trans_matrix_valid and self.__rotation_matrix_valid and self.__scale_matrix_valid:
             return self.__matrix
         else:
+            self.__update_matrices()
             self.__matrix = self.__trans_matrix @ self.__rotation_matrix @ self.__scale_matrix
-            self.__matrix_valid = True
             return self.__matrix
+
+    @property
+    def transformation_matrix(self):
+        if self.__parent is None:
+            return self.matrix
+        else:
+            return self.__parent.matrix @ self.matrix
+
+    @property
+    def parent(self):
+        return self.__parent
+
+    @parent.setter
+    def parent(self, obj):
+        last_parent = self.__parent
+        self.__parent = obj
+        if last_parent is not None:
+            last_parent.remove_child(self)
+        if self.__parent is not None:
+            self.__parent.add_child(self)
+
+    @property
+    def children(self):
+        return self.__children
+
+    def add_child(self, child):
+        self.__children.add(child)
+        if child.parent != self:
+            child.parent = self
+
+    def remove_child(self, child):
+        child in self.__children and self.__children.remove(child)
+        if child.parent == self:
+            child.parent = None
+
+    # def __get_descendants(self, curr_descendants):
+    #     descendants = set()
+    #     for child in self.__children.difference(curr_descendants):
+    #         child_desc = child.__get_descendants(descendants)
+    #         descendants.union(child_desc)
+    #     return descendants
+
+    # def get_descendants(self):
+    #     descendants = {self}
+    #     descendants.union(self.children)
+    #     for child in self.children:
+    #         child_desc = child.__get_descendants(descendants)
+    #         descendants.union(child_desc)
+    #     descendants.remove(self)
+    #     return descendants
+
+    def get_descendants(self):
+        return {desc for child in self.children for desc in child.get_descendants() | self.children}
 
     @property
     def translation(self):
@@ -34,8 +103,7 @@ class GameObject:
     @translation.setter
     def translation(self, val):
         self.__translation = val
-        self.__trans_matrix = pyrr.matrix44.create_from_translation(val)
-        self.__matrix_valid = False
+        self.__trans_matrix_valid = False
 
     @property
     def scale(self):
@@ -44,8 +112,7 @@ class GameObject:
     @scale.setter
     def scale(self, val):
         self.__scale = val
-        self.__scale_matrix = pyrr.matrix44.create_from_scale(val)
-        self.__matrix_valid = False
+        self.__scale_matrix_valid = False
 
     @property
     def rotation(self):
@@ -54,5 +121,4 @@ class GameObject:
     @rotation.setter
     def rotation(self, val):
         self.__rotation = val
-        self.__rotation_matrix = pyrr.matrix44.create_from_eulers(val)
-        self.__matrix_valid = False
+        self.__rotation_matrix_valid = False
