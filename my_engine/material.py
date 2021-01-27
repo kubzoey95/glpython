d844a5b3b07d22807239e43d8a22faa0e3ead485
Jakub Belicki <jakubsliwice@o2.pl>
2021-01-27 23:01:02 +0100

    some materials and meshes stuff

--- my_engine/material.py
+++ my_engine/material.py
@@ -1,26 +1,54 @@
 from my_engine.component import Component
-from typing import Union
+from typing import Union, Dict, NamedTuple
+from collections import namedtuple
+import numpy as np
 from OpenGL.GL.shaders import compileProgram, compileShader
-from OpenGL.GL import GL_VERTEX_SHADER, GL_FRAGMENT_SHADER, GL_ACTIVE_ATTRIBUTES,\
-    glGetProgramiv, glGetActiveAttrib, GLuint, GLenum, GLint, GLsizei, GLchar, glGetActiveUniform, GL_ACTIVE_UNIFORMS
+from OpenGL.GL import GL_VERTEX_SHADER, GL_FRAGMENT_SHADER, GL_ACTIVE_ATTRIBUTES, \
+    glGetProgramiv, glGetActiveAttrib, GLuint, GLenum, GLint, GLsizei, GLchar, glGetActiveUniform, GL_ACTIVE_UNIFORMS, \
+    glGetUniformLocation, glGetAttribLocation, GL_GEOMETRY_SHADER
 
 
 class Material(Component):
-    def __init__(self, vertex_shader: Union[str, None] = None, fragment_shader: Union[str, None] = None, obj=None):
+    def __init__(self, vertex_shader: Union[str, None] = None, fragment_shader: Union[str, None] = None,
+                 geometry_shader: Union[str, None] = None, obj=None):
         super().__init__('Material', obj)
         self.__vertex_shader = vertex_shader
         self.__fragment_shader = fragment_shader
+        self.__geometry_shader = geometry_shader
+        self.__attributes: Dict[str, int] = {}
+        self.__uniforms: Dict[str, int] = {}
+
+        self.__attributes_values: Union[NamedTuple[np.ndarray], None] = None
+        self.__uniforms_values: Union[NamedTuple[np.ndarray], None] = None
 
         if self.__vertex_shader and self.__fragment_shader:
-            program = compileProgram(compileShader(vertex_shader, GL_VERTEX_SHADER), compileShader(fragment_shader, GL_FRAGMENT_SHADER))
+            program = compileProgram(
+                *[compileShader(shader, const) for shader, const in [(self.__vertex_shader, GL_VERTEX_SHADER),
+                                                                     (self.__fragment_shader, GL_FRAGMENT_SHADER),
+                                                                     (self.__geometry_shader, GL_GEOMETRY_SHADER)] if shader]
+            )
             self.__program = program
 
             num_active_attribs = glGetProgramiv(program, GL_ACTIVE_ATTRIBUTES)
             for u in range(num_active_attribs):
                 name, size, type_ = glGetActiveAttrib(program, u)
-                print(name, size, type_)
+                location = glGetAttribLocation(program, name)
+                self.__attributes[name.decode("utf-8")] = location
+
+            self.__attributes_values = namedtuple('ShaderAttributes', self.__attributes.keys())(*[None for _ in range(len(self.__attributes))])
 
             num_active_uniforms = glGetProgramiv(program, GL_ACTIVE_UNIFORMS)
             for u in range(num_active_uniforms):
                 name, size, type_ = glGetActiveUniform(program, u)
-                print(name, size, type_)
+                location = glGetUniformLocation(program, name)
+                self.__uniforms[name.decode("utf-8")] = location
+
+            self.__uniforms_values = namedtuple('ShaderUniforms', self.__uniforms.keys())(*[None for _ in range(len(self.__uniforms))])
+
+    @property
+    def attributes(self):
+        return self.__attributes_values
+
+    @property
+    def uniforms(self):
+        return self.__uniforms_values
--- my_engine/mesh.py
+++ my_engine/mesh.py
@@ -1,12 +1,35 @@
 from my_engine.component import Component
 import numpy as np
 import meshio
+from typing import Union, NamedTuple, Dict
+from collections import namedtuple
+import re
 
 
 class Mesh(Component):
-    def __init__(self, vertices=np.array([]), obj=None):
+    def __init__(self, vertices: np.ndarray = np.array([]), indices: np.ndarray = np.array([]),
+                 point_data: Union[Dict[str, np.ndarray], None] = None, obj=None):
         super().__init__('Mesh', obj)
-        self.vertices: np.ndarray = vertices
+        self.__vertices: np.ndarray = vertices
+        self.__indices: np.ndarray = indices
+        self.__point_data: Union[NamedTuple[np.ndarray], None] = None
+        if point_data is not None:
+            self.__point_data = namedtuple('PointData', point_data.keys())(*point_data.values())
 
     def load_from_file(self, file):
         mesh_obj = meshio.read(file)
+        self.__vertices = mesh_obj.points
+        self.__indices = mesh_obj.cells_dict['triangle']
+        self.__point_data = namedtuple('PointData', [re.sub('[^0-9a-zA-Z_]', '', key) for key in mesh_obj.point_data.keys()])(*mesh_obj.point_data.values())
+
+    @property
+    def vertices(self):
+        return self.__vertices
+
+    @property
+    def indices(self):
+        return self.__indices
+
+    @property
+    def point_data(self):
+        return self.__point_data
