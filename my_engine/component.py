4cf4c319dc675eb9d3dbe22599318ae27e483e72
Jakub Belicki <jakubsliwice@o2.pl>
2021-01-27 13:34:39 +0100

    Update component.py

--- my_engine/component.py
+++ my_engine/component.py
@@ -1,9 +1,20 @@
 class Component:
-    def __init__(self):
-        pass
+    def __init__(self, name: str, obj=None):
+        self.__name: str = name
+        self.__obj = obj
+        if self.__obj is not None:
+            self.__obj.add_component(self)
+
+    @property
+    def obj(self):
+        return self.__obj
+
+    @property
+    def name(self):
+        return self.__name
 
     def start(self):
-        raise NotImplementedError
+        pass
 
     def update(self):
-        raise NotImplementedError
+        pass
